6a72b25e7eaeb17b3ae186c3a182e28c2e051afa
Julian <julian@juliantomlindesign.com>
2019-04-08 18:58:24 +0000

    finished step 1, get and print

---
+++ 01_print_chunks/get_and_print.js
@@ -0,0 +1,22 @@
+var https = require('https');
+
+function getAndPrintHTMLChunks () {
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+
+  https.get(requestOptions, function (response) {
+
+    response.setEncoding('utf8');
+
+    response.on('data', function (chunk) {
+      console.log(chunk + '\n')
+    })
+
+  })
+
+}
+
+getAndPrintHTMLChunks()
